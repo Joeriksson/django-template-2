1d7fdb3f48f6be6d88fdea666d043da2ed088ec8
J-O Eriksson <25037079+Joeriksson@users.noreply.github.com>
2022-06-05 17:06:06 +0000

    add CSRF_TRUSTED_ORIGINS

--- project/settings/base.py
+++ project/settings/base.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*.githubpreview.dev']
 #ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = []
 # Application definition
